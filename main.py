8b3b0a264617bf117d3c25dbdd8188823282f7b7
omid sakaki ghazvini <103527073+omid-sakaki-ghazvini@users.noreply.github.com>
2024-08-21 23:15:14 +0330

    Update main.py

--- main.py
+++ main.py
@@ -3,8 +3,8 @@ from transformers import AutoModelForQuestionAnswering, AutoTokenizer, pipeline
 import streamlit as st
 
 model_name_or_path = "m3hrdadfi/xlmr-large-qa-fa"
-pdfFilePath = './dr_ghalibaf.pdf'
-pathFileForModel = './document_for_chatBot.txt'
+pdfFilePath = 'dr_ghalibaf.pdf'
+pathFileForModel = 'document_for_chatBot.txt'
 
 nlp = pipeline('question-answering', model=model_name_or_path, tokenizer=model_name_or_path)
 
