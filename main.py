49d36d4f51a103d5d6562bd1d36297eb4615d753
omid sakaki ghazvini <103527073+omid-sakaki-ghazvini@users.noreply.github.com>
2024-08-22 18:28:58 +0330

    Update main.py

--- main.py
+++ main.py
@@ -1,9 +1,12 @@
-from transformers import pipeline
+from transformers import pipeline, AutoTokenizer, AutoModelForQuestionAnswering
 import streamlit as st
 
 pathFileForModel = 'document_for_chatBot.txt'
-model_name_or_path = "m3hrdadfi/xlmr-large-qa-fa"
-nlp = pipeline('question-answering', model=model_name_or_path)
+
+tokenizer = AutoTokenizer.from_pretrained("m3hrdadfi/xlmr-large-qa-fa")
+model = AutoModelForQuestionAnswering.from_pretrained("m3hrdadfi/xlmr-large-qa-fa")
+pipe = pipeline("question-answering", model=model, tokenizer=tokenizer )
+
 
 st.title("سلام، من یک هوش مصنوعی هستم که میتوانم به سوال شما در رابطه با کارنامه دکتر قالیباف پاسخ بدهم")
 
@@ -22,7 +25,7 @@ question = st.text_area("لطفا سوال خود را بپرسید:")
 
 def chatBot(question, context):
     kwargs = {}
-    r = nlp(question=question, context=context, **kwargs)    
+    r = pipe(question=question, context=context, **kwargs)    
     answer = " ".join([token.strip() for token in r["answer"].strip().split() if token.strip()]) 
     return(answer)  
       
