0ac5a840eb47feaa6298b284770e51c96579b7d8
omid sakaki ghazvini <103527073+omid-sakaki-ghazvini@users.noreply.github.com>
2024-08-22 17:40:28 +0330

    Update main.py

--- main.py
+++ main.py
@@ -1,27 +1,11 @@
-from langchain_community.document_loaders import PyPDFium2Loader
 from transformers import AutoModelForQuestionAnswering, AutoTokenizer, pipeline
 import streamlit as st
 
-pdfFilePath = 'dr_ghalibaf.pdf'
 pathFileForModel = 'document_for_chatBot.txt'
 
 pipeline = pipeline(task= 'question-answering', model="m3hrdadfi/xlmr-large-qa-fa", tokenizer="m3hrdadfi/xlmr-large-qa-fa")
 
 st.title("سلام، من یک هوش مصنوعی هستم که میتوانم به سوال شما در رابطه با کارنامه دکتر قالیباف پاسخ بدهم")
-
-def dataLoad(pdfFilePath):
-    loader = PyPDFium2Loader(pdfFilePath) 
-    data = loader.load()
-    context = []
-    for i in range(len(data)):
-        context.append(data[i].page_content)
-    file = open(pathFileForModel , mode='w', encoding='utf-8')    
-    for j in range(len(context)):
-      file.writelines(context[j])  
-    file.close()
-    loadData = False 
-    return()
-
 
 if loadData :
     dataLoad(pdfFilePath)
