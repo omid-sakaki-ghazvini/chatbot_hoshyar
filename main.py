de39e5b962e1a32cd64cd516962acfbcf2ba8100
omid sakaki ghazvini <103527073+omid-sakaki-ghazvini@users.noreply.github.com>
2024-08-22 21:05:43 +0330

    Update main.py

--- main.py
+++ main.py
@@ -7,8 +7,7 @@ tokenizer = AutoTokenizer.from_pretrained("m3hrdadfi/xlmr-large-qa-fa")
 model = AutoModelForQuestionAnswering.from_pretrained("m3hrdadfi/xlmr-large-qa-fa")
 pipe = pipeline("question-answering", model=model, tokenizer=tokenizer )
 
-original_title = '<p style={{font-family:"Courier" color:"Blue" font-size: "20px"}}>سلام، من یک هوش مصنوعی هستم که میتوانم به سوال شما در رابطه با کارنامه دکتر قالیباف پاسخ بدهم</p>'
-st.markdown(original_title, unsafe_allow_html=True)
+st.markdown('<p class="big-font">سلام، من یک هوش مصنوعی هستم که میتوانم به سوال شما در رابطه با کارنامه دکتر قالیباف پاسخ بدهم</p>', unsafe_allow_html=True)
 
 with open(pathFileForModel, 'r', encoding='utf-8') as file:
     context = file.readlines()
